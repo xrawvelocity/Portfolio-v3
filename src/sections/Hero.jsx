4729b9d365092f594573481c54112ccde1914d6f
xrawvelocity <vmfs99@gmail.com>
2023-03-19 16:48:31 -0400

    undid last change

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,22 +6,15 @@ const Hero = () => {
         <div className="hero">
             <div className="hero-text">
                 <h2 className="hero-text-title">
-                    Victor Fernandez
+                    Welcome to my
                     <br />
-                    <span>Web Developer</span>
+                    <span>Portfolio</span>
                 </h2>
                 <p className="hero-text-subtitle">
-                    Welcome to my Portfolio. As a skilled web developer with a
-                    track record of delivering high-quality websites and
-                    solutions, I have been helping businesses and individuals
-                    achieve their online goals since 2019. Whether you're a
-                    potential employer or a freelance client, take a look at my
-                    work and let's discuss how I can help bring your vision to
-                    life.
-                    {/* My name is Victor Fernandez and I've been crafting creative
+                    My name is Victor Fernandez and I've been crafting creative
                     and result-&#8288;oriented websites for
                     forward-&#8288;thinking brands,
-                    like&nbsp;yours,&nbsp;since&nbsp;2019. */}
+                    like&nbsp;yours,&nbsp;since&nbsp;2019.
                 </p>
             </div>
             <div className="hero-image">
